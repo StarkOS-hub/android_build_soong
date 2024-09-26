2fa25c21a60eaa13082e60fd9de82c1da1b9ba0b
StarkOS <starkos.git@gmail.com>
2024-09-27 01:24:21 +0530

    Update dumpvars.go

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -140,7 +140,7 @@ var BannerVars = []string{
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
 	"CR_VERSION",
-	"MATRIXX_BUILD_INFO",
+	"STARK_BUILD_INFO",
 	"STARK_MAINTAINER",
 	"PRODUCT_INCLUDE_TAGS",
 	"TARGET_DEVICE",
@@ -192,7 +192,7 @@ func Banner(make_vars map[string]string) string {
 		fmt.Fprintln(b, "==========================================================")
 		fmt.Fprintln(b, "               StarkOS by JigenxOhtsusuki                 ")
 		fmt.Fprintln(b, "==========================================================")
-		fmt.Fprintf(b, "%s=%s\n", "STARK_BUILD_INFO", make_vars["MATRIXX_BUILD_INFO"])
+		fmt.Fprintf(b, "%s=%s\n", "STARK_BUILD_INFO", make_vars["STARK_BUILD_INFO"])
 		fmt.Fprintf(b, "%s=%s\n", "PLATFORM_VERSION", make_vars["PLATFORM_VERSION"])
 	        fmt.Fprintf(b, "%s=%s\n", "STARK_MAINTAINER", make_vars["STARK_MAINTAINER"])
 		fmt.Fprintf(b, "%s=%s\n", "STARK_VERSION", make_vars["CR_VERSION"])
